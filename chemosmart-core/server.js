3ddd5fa05c8bc545a79dee2c87c5e23dbb430e5e
GiuseppeBasileRyuzaki <giuseppebasile522@gmail.com>
2023-01-10 15:58:25 +0100

    Aggiornata tabella, adesso responsive e colori del sito

--- chemosmart-core/server.js
+++ chemosmart-core/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const port = 3000
+const port = 3001
 
 app.set('view engine', 'ejs')
 app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
