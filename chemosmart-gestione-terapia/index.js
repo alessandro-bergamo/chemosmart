639f623834194c3703749b16f2de6cd1c8d2cf19
AntonioNappi988 <74452384+AntonioNappi988@users.noreply.github.com>
2023-01-28 17:44:40 +0100

    Creata connessione a MongoDB, Creata funzione che restituisce tutte le terapie nel DB

---
+++ chemosmart-gestione-terapia/index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+
+//prende le routes
+const terapiaRoutes = require('./routes/terapia.js')
+
+//cors servirà in futurto quando collegheremo tutto al frontend e per le varie orgin esterne. per chiarimenti vedere info su CORS
+//const cors = require('cors')
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+const CONNECTION_DB_URL = 'mongodb+srv://admin:admin@chemosmart.ce52soe.mongodb.net/Terapia?retryWrites=true&w=majority'
+
+//midlware per gestire il body della request
+app.use(express.json())
+
+//usa cors
+//app.use(cors())
+
+//usa le routes
+app.use('/terapie', terapiaRoutes)
+
+app.get('/', (req,res)=>{
+    res.send("funziona")
+})
+
+//connesione a mongo
+/* mongoose.set('strictQuery', false) */    //questo serve per eliminare il warning quando esegui il progetto, però per adesso non lo voglio gestire
+mongoose.connect(CONNECTION_DB_URL)
+.then(()=>{
+    console.log('connessione al db...')
+    app.listen(PORT, () =>console.log(`Server running on port: http://localhost:${PORT}`));
+})
+.catch(error => console.error(error))
+
+
