88881f5111bba4c9fb25c5e154a1f00e49dc55d6
Luigi <luigi.miranda998@gmail.com>
2023-01-30 14:27:16 +0100

    Creata cartella routes, con le routes per tutte le richieste crud per gli appuntamenti. creato fille index js con connessione a mongo e homeroute del microservizio

--- chemosmart-gestioneAppuntamenti/express-mongodb/modules/Appuntamento.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const schema = new mongoose.Schema({
-    data: Date,
-    ora: String,
-    paziente: String
-});
-
-module.exports = mongoose.model("Appuntamento",schema);
