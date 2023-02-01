ae81bdce5f12f211facfce72d90a01fa65aeaa45
AntonioNappi988 <74452384+AntonioNappi988@users.noreply.github.com>
2023-02-01 19:04:13 +0100

    Realizzata pagina e form Aggiungi terapia, e collegamento back-end tramite libreria Axios

--- chemosmart-core/server.js
+++ chemosmart-core/server.js
@@ -1,16 +1,20 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const port = 3000
+const port = 3003
+const axios = require('axios')
+const bodyParser = require('body-parser')
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended : true}))
 app.set('view engine', 'ejs')
 app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
 app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
 app.use('/images', express.static(path.resolve(__dirname, "assets/images")))
 // switcha il commento per cambiare sidebar visualizzata (usato per testare se tutto va)
-// let user = 'medico'
+let user = 'medico'
 // let user = 'infermiere'
-let user = 'segretario'
+//let user = 'segretario'
 
 app.get('/', (req, res) => {
     res.render(__dirname + '/views/loginPage')
@@ -43,3 +47,13 @@ app.listen(port, () => {
     console.log(`Server listening on http://localhost:${port}`)
 })
 
+app.get('/aggiungiTerapia', (req, res) => {
+    res.render(__dirname + "/views/aggiungiTerapia")
+})
+
+app.post('/addTerapia',(req, res) => {
+    axios.post("http://localhost:3045/terapie" , req.body)
+    .then(function(response){
+         res.send("Terapia Aggiunta")
+    })
+ })
