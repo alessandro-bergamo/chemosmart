7d0093ba5de74ebb3d3662e61f03dc0fc9c4d8ec
Ciro Troiano <85676063+ImCirot@users.noreply.github.com>
2023-02-04 11:56:51 +0100

    Added new info on paziente schema

--- chemosmart-gestionePazienti/Pazienti.js
+++ chemosmart-gestionePazienti/Pazienti.js
@@ -6,7 +6,118 @@ const schema = new mongoose.Schema({
     telefono: String,
     email: String,
     sesso: String,
-    dataNascita: Date
+    dataNascita: Date,
+    Eta: Number,
+    "indice inquinamento ambientale": {
+        type: Number,
+        min: 0,
+        max: 10
+    },
+    "indice uso alcolici": {
+        type: Number,
+        min: 0,
+        max: 10
+    },
+    "grado di allergia": {
+        type: Number,
+        min: 0,
+        max: 10,
+    },
+    "grado di rischio lavorativo": {
+        type: Number,
+        min: 0,
+        max: 10,
+    },
+    "indice dei fattori di rischio familiare": {
+        type: Number,
+        min: 0,
+        max: 10,
+    },
+    "indice di malattie croniche": {
+        type: Number,
+        min: 0,
+        max: 10,
+    },
+    "indice di alimentazione scorretta": {
+        type: Number,
+        min: 0,
+        max: 10,
+    },
+    "indice di obesita": {
+        type: Number,
+        min: 0,
+        max: 10,
+    },
+    "grado di esposizione a fumo attivo": {
+        type: Number,
+        min: 0,
+        max: 10,
+    },
+    "grado di esposizione a fumo passivo": {
+        type: Number,
+        min: 0,
+        max: 10,
+    },
+    "indice di dolori localizzato": {
+        type: Number,
+        min: 0,
+        max: 10,
+    },
+    "indice emottisi": {
+        type: Number,
+        min: 0,
+        max: 10,
+    },
+    "indice astenia": {
+        type: Number,
+        min: 0,
+        max: 10,
+    },
+    "indice di perdita di peso": {
+        type: Number,
+        min: 0,
+        max: 10,
+    },
+    "indice dispnea": {
+        type: Number,
+        min: 0,
+        max: 10,
+    },
+    "indice respiro sibilante": {
+        type: Number,
+        min: 0,
+        max: 10,
+    },
+    "indice disfagia": {
+        type: Number,
+        min: 0,
+        max: 10,
+    },
+    "stato dita di Ippocrate": {
+        type: Number,
+        min: 0,
+        max: 10,
+    },
+    "stato di immunodepressione": {
+        type: Number,
+        min: 0,
+        max: 10,
+    },
+    "indice tosse secca": {
+        type: Number,
+        min: 0,
+        max: 10,
+    },
+    "indice di russamento": {
+        type: Number,
+        min: 0,
+        max: 10,
+    },
+    priorita: {
+        type: String,
+        enum: ['Alta','Media','Bassa','None'],
+        default: 'None'
+    }
 });
 
 module.exports = mongoose.model("PazientiSistema",schema);
--- chemosmart-gestionePazienti/index.js
+++ chemosmart-gestionePazienti/index.js
@@ -20,8 +20,30 @@ mongoose
             cf: "De Palma",
             telefono: "10",
             email: "c.depalma5@studenti.unisa.it",
-            sesso: "3452423623",
-            dataNascita: "2000-02-12"
+            sesso: "M",
+            dataNascita: "2000-02-12",
+            Eta: 33,
+            "indice inquinamento ambientale": 2,
+            "indice uso alcolici": 4,
+            "grado di allergia": 5,
+            "grado di rischio lavorativo": 4,
+            "indice dei fattori di rischio familiare": 3,
+            "indice di malattie croniche": 2,
+            "indice di alimentazione scorretta": 2,
+            "indice di obesita": 4,
+            "grado di esposizione a fumo attivo": 3,
+            "grado di esposizione a fumo passivo": 2,
+            "indice di dolori localizzati": 2,
+            "indice emottisi": 4,
+            "indice astenia": 3,
+            "indice di perdita di peso": 4,
+            "indice dispnea": 2,
+            "indice respiro sibilante": 2,
+            "indice disfagia": 3,
+            "stato dita di Ippocrate": 1,
+            "stato di immunodepressione": 2,
+            "indice tosse secca": 3,
+            "indice di russamento": 4
             }))
         });
         app.get("/getPazienti", async (req, res) => {
