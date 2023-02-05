1691b80647dd64162d61281605d1f85e61d9c305
Ciro Troiano <85676063+ImCirot@users.noreply.github.com>
2023-02-05 10:54:53 +0100

    Added modals for filtri and homepage medico

--- chemosmart-core/server.js
+++ chemosmart-core/server.js
@@ -8,9 +8,9 @@ app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
 app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
 app.use('/images', express.static(path.resolve(__dirname, "assets/images")))
 // switcha il commento per cambiare sidebar visualizzata (usato per testare se tutto va)
-// let user = 'medico'
+let user = 'medico'
 // let user = 'infermiere'
-let user = 'segretario'
+// let user = 'segretario'
 
 app.get('/', (req, res) => {
     res.render(__dirname + '/views/loginPage')
