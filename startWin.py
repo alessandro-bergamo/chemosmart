dc0460d0b5eb188681bd4101d849c61a03134f66
Luigi Miranda <100714371+luigiMiranda@users.noreply.github.com>
2023-02-09 00:10:56 +0100

    Craeati script python per installare dipendenze di tutti i microservizi, startare insieme tutti i server

---
+++ startWin.py
@@ -0,0 +1,10 @@
+import os
+import subprocess
+
+folder_path = r'C:\Users\Luigi\Desktop\progettoIS\chemosmart'
+
+for item in os.listdir(folder_path):
+    item_path = os.path.join(folder_path, item)
+    if os.path.isdir(item_path) and os.path.isfile(os.path.join(item_path, 'package.json')):
+        os.chdir(item_path)
+        subprocess.Popen(['C:\\Program Files\\nodejs\\npm.cmd', 'start'])
