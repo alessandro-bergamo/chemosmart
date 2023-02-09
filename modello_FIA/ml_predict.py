77585d66311c31ad7b46c606569a6bc323ff4789
Ciro Troiano <85676063+ImCirot@users.noreply.github.com>
2023-02-09 15:12:56 +0100

    added some comments to new code

--- modello_FIA/ml_predict.py
+++ modello_FIA/ml_predict.py
@@ -15,6 +15,8 @@ def predict(file_name):
     #predizione e restituzione del valore predetto al chiamante
     pred = loaded_module.predict(patient)
     
+    #viene stampato sul stdout il valore della predizione 
+    # (in italiano per poterlo utilizzare all'interno della webApp)
     if(pred[0] == "High"):
         print("Alta")
     elif(pred[0] == "Medium"):
@@ -27,5 +29,8 @@ def predict(file_name):
 # print(f'La predizione è: {pred}')
 
 if __name__ == "__main__":
+    #quando viene eseguito lo script viene richiamata la funzione per la predizione
+    #passando come argomento da linea di comando il nome del file in cui sono memorizzati
+    #i dati del paziente
     predict(sys.argv[1])
     
