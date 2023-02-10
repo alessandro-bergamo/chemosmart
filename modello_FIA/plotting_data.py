405b77e98ce0e962d06dd2e66abc02e33c037d58
Ciro Troiano <85676063+ImCirot@users.noreply.github.com>
2023-02-10 11:05:53 +0100

    updated plotting data to match dataset updated columns

--- modello_FIA/plotting_data.py
+++ modello_FIA/plotting_data.py
@@ -5,7 +5,7 @@ import seaborn as sns
 
 def plotting_priorities(data):
     sns.set_style('whitegrid')
-    sns.histplot(data=data, x='Level')
+    sns.histplot(data=data, x='priorita')
 
     plt.title('Numero di pazienti per livello di priorita')
     plt.ylabel('Numero di casi')
@@ -15,15 +15,15 @@ def plotting_priorities(data):
 
 def plotting_priorities_on_ages(data):
     #viene scremato il dataset selezionando solamente i pazienti con priorità elevata
-    high_level_patients = data.loc[data['Level'] == 'High']
-    medium_level_patients = data.loc[data['Level'] == 'Medium']
-    low_level_patients = data.loc[data['Level'] == 'Low']
+    high_level_patients = data.loc[data['priorita'] == 'High']
+    medium_level_patients = data.loc[data['priorita'] == 'Medium']
+    low_level_patients = data.loc[data['priorita'] == 'Low']
 
     #set stile del grafico (mostra grid nel grafico)
     sns.set_style('whitegrid')
 
     #grafico che mostra rapporto priorità alta/età
-    sns.histplot(data=high_level_patients, x='Age')
+    sns.histplot(data=high_level_patients, x='eta')
 
     #set del titolo del grafico, label asse y e asse x
     plt.title("Numero di pazienti a priorità alta per range di età")
@@ -34,7 +34,7 @@ def plotting_priorities_on_ages(data):
     plt.show()
 
     #grafico che mostra rapporto priorità media/età
-    sns.histplot(data=medium_level_patients, x='Age')
+    sns.histplot(data=medium_level_patients, x='eta')
 
     #set del titolo del grafico, label asse y e asse x
     plt.title("Numero di pazienti a priorità media per range di età")
@@ -45,7 +45,7 @@ def plotting_priorities_on_ages(data):
     plt.show()
 
     #grafico che mostra rapporto priorità bassa/età
-    sns.histplot(data=low_level_patients, x='Age')
+    sns.histplot(data=low_level_patients, x='eta')
 
     #set del titolo del grafico, label asse y e asse x
     plt.title("Numero di pazienti a priorità bassa per range di età")
@@ -56,12 +56,12 @@ def plotting_priorities_on_ages(data):
     plt.show()
 
 def plotting_priorities_on_alcohol_use(data):
-    high_level_patients = data.loc[data['Level'] == 'High']
-    medium_level_patients = data.loc[data['Level'] == 'Medium']
-    low_level_patients = data.loc[data['Level'] == 'Low']
+    high_level_patients = data.loc[data['priorita'] == 'High']
+    medium_level_patients = data.loc[data['priorita'] == 'Medium']
+    low_level_patients = data.loc[data['priorita'] == 'Low']
 
     #grafico che mostra rapporto fra uso di alcool/priorità alta
-    sns.histplot(data=high_level_patients, x='Alcohol use')
+    sns.histplot(data=high_level_patients, x='indice_uso_alcolici')
 
     #set del titolo del grafico, label asse y e asse x
     plt.title("Numero di pazienti a priorità alta per indice uso di alcool")
@@ -72,7 +72,7 @@ def plotting_priorities_on_alcohol_use(data):
     plt.show()
 
     #grafico che mostra rapporto fra uso di alcool/priorità media
-    sns.histplot(data=medium_level_patients, x='Alcohol use')
+    sns.histplot(data=medium_level_patients, x='indice_uso_alcolici')
 
     #set del titolo del grafico, label asse y e asse x
     plt.title("Numero di pazienti a priorità media per indice uso di alcool")
@@ -83,7 +83,7 @@ def plotting_priorities_on_alcohol_use(data):
     plt.show()
 
     #grafico che mostra rapporto fra uso di alcool/priorità bassa
-    sns.histplot(data=low_level_patients, x='Alcohol use')
+    sns.histplot(data=low_level_patients, x='indice_uso_alcolici')
 
     #set del titolo del grafico, label asse y e asse x
     plt.title("Numero di pazienti a priorità bassa per indice uso di alcool")
@@ -94,12 +94,12 @@ def plotting_priorities_on_alcohol_use(data):
     plt.show()
 
 def plotting_priorities_on_genetic_risk(data):
-    high_level_patients = data.loc[data['Level'] == 'High']
-    medium_level_patients = data.loc[data['Level'] == 'Medium']
-    low_level_patients = data.loc[data['Level'] == 'Low']
+    high_level_patients = data.loc[data['priorita'] == 'High']
+    medium_level_patients = data.loc[data['priorita'] == 'Medium']
+    low_level_patients = data.loc[data['priorita'] == 'Low']
 
     #grafico che mostra rapporto fra fattore di rischio familiare/priorità alta
-    sns.histplot(data=high_level_patients, x='Genetic Risk')
+    sns.histplot(data=high_level_patients, x='indice_fattori_rischio_familiare')
 
     #set del titolo del grafico, label asse y e asse x
     plt.title("Numero di pazienti a priorità alta per fattore di rischio familiare")
@@ -110,7 +110,7 @@ def plotting_priorities_on_genetic_risk(data):
     plt.show()
 
     #grafico che mostra rapporto fra fattore di rischio familiare/priorità media
-    sns.histplot(data=medium_level_patients, x='Genetic Risk')
+    sns.histplot(data=medium_level_patients, x='indice_fattori_rischio_familiare')
 
     #set del titolo del grafico, label asse y e asse x
     plt.title("Numero di pazienti a priorità media per fattore di rischio familiare")
@@ -121,7 +121,7 @@ def plotting_priorities_on_genetic_risk(data):
     plt.show()
 
     #grafico che mostra rapporto fra fattore di rischio familiare/priorità bassa
-    sns.histplot(data=low_level_patients, x='Genetic Risk')
+    sns.histplot(data=low_level_patients, x='indice_fattori_rischio_familiare')
 
     #set del titolo del grafico, label asse y e asse x
     plt.title("Numero di pazienti a priorità bassa per fattore di rischio familiare")
@@ -132,12 +132,12 @@ def plotting_priorities_on_genetic_risk(data):
     plt.show()
 
 def plotting_priorities_on_smoking(data):
-    high_level_patients = data.loc[data['Level'] == 'High']
-    medium_level_patients = data.loc[data['Level'] == 'Medium']
-    low_level_patients = data.loc[data['Level'] == 'Low']
+    high_level_patients = data.loc[data['priorita'] == 'High']
+    medium_level_patients = data.loc[data['priorita'] == 'Medium']
+    low_level_patients = data.loc[data['priorita'] == 'Low']
 
     #grafico che mostra rapporto fra esposizione a fumo attivo (fumatore)/priorità alta
-    sns.histplot(data=high_level_patients, x='Smoking')
+    sns.histplot(data=high_level_patients, x='grado_esposizione_fumo_attivo')
 
     #set del titolo del grafico, label asse y e asse x
     plt.title("Numero di pazienti a priorità alta per esposizione a fumo attivo (fumatore)")
@@ -148,7 +148,7 @@ def plotting_priorities_on_smoking(data):
     plt.show()
 
     #grafico che mostra rapporto fra esposizione a fumo attivo (fumatore)/priorità media
-    sns.histplot(data=medium_level_patients, x='Smoking')
+    sns.histplot(data=medium_level_patients, x='grado_esposizione_fumo_attivo')
 
     #set del titolo del grafico, label asse y e asse x
     plt.title("Numero di pazienti a priorità media per esposizione a fumo attivo (fumatore)")
@@ -159,7 +159,7 @@ def plotting_priorities_on_smoking(data):
     plt.show()
 
     #grafico che mostra rapporto fra esposizione a fumo attivo (fumatore)/priorità bassa
-    sns.histplot(data=low_level_patients, x='Smoking')
+    sns.histplot(data=low_level_patients, x='grado_esposizione_fumo_attivo')
 
     #set del titolo del grafico, label asse y e asse x
     plt.title("Numero di pazienti a priorità bassa per esposizione a fumo attivo (fumatore)")
@@ -170,12 +170,12 @@ def plotting_priorities_on_smoking(data):
     plt.show()
 
 def plotting_priorities_on_unbalanced_diet(data):
-    high_level_patients = data.loc[data['Level'] == 'High']
-    medium_level_patients = data.loc[data['Level'] == 'Medium']
-    low_level_patients = data.loc[data['Level'] == 'Low']
+    high_level_patients = data.loc[data['priorita'] == 'High']
+    medium_level_patients = data.loc[data['priorita'] == 'Medium']
+    low_level_patients = data.loc[data['priorita'] == 'Low']
 
     #grafico che mostra rapporto fra alimentazione scorretta/priorità alta
-    sns.histplot(data=high_level_patients, x='Unbalanced Diet')
+    sns.histplot(data=high_level_patients, x='indice_alimentazione_scorretta')
 
     #set del titolo del grafico, label asse y e asse x
     plt.title("Numero di pazienti a priorità alta per alimentazione scorretta")
@@ -186,7 +186,7 @@ def plotting_priorities_on_unbalanced_diet(data):
     plt.show()
 
     #grafico che mostra rapporto fra alimentazione scorretta/priorità media
-    sns.histplot(data=medium_level_patients, x='Unbalanced Diet')
+    sns.histplot(data=medium_level_patients, x='indice_alimentazione_scorretta')
 
     #set del titolo del grafico, label asse y e asse x
     plt.title("Numero di pazienti a priorità media per alimentazione scorretta")
@@ -197,7 +197,7 @@ def plotting_priorities_on_unbalanced_diet(data):
     plt.show()
 
     #grafico che mostra rapporto fra alimentazione scorretta/priorità bassa
-    sns.histplot(data=low_level_patients, x='Unbalanced Diet')
+    sns.histplot(data=low_level_patients, x='indice_alimentazione_scorretta')
 
     #set del titolo del grafico, label asse y e asse x
     plt.title("Numero di pazienti a priorità bassa per alimentazione scorretta")
