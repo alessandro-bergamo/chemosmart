9280689fd7935dfaf0ff384c338a4107d5598948
GiuseppeBasileRyuzaki <giuseppebasile522@gmail.com>
2023-02-17 17:39:30 +0100

    Aggiunta funzione di test per UpdateFarmaco

--- chemosmart-gestione-farmaci/tests/farmaci.test.js
+++ chemosmart-gestione-farmaci/tests/farmaci.test.js
@@ -1,8 +1,8 @@
-const { deleteFarmaco } = require('../controllers/farmaci') 
+const { deleteFarmaco, updateFarmaco } = require('../controllers/farmaci') 
 const Farmaco = require('../models/farmaco')
 
 describe('deleteFarmaco', () => {
-    test('deve cancellare un Farmaco con l\'id passato e restituire un messaggio di successo', async () => {
+    test('Dovrebbe cancellare un Farmaco con l\'id passato e restituire un messaggio di successo', async () => {
         const id = '123'
         const req = {
             params: {
@@ -20,7 +20,7 @@ describe('deleteFarmaco', () => {
         expect(res.json).toHaveBeenCalledWith({ message: 'Farmaco eliminato con successo' })
     })
 
-    test('in caso di Farmaco non trovato imposta stato richiesta come 400 e restituisce errore', async () => {
+    test('Dovrebbe impostare stato richiesta codice 404 e restituire errore in caso di Farmaco non trovato', async () => {
         const id = '123'
         const req = {
             params: {
@@ -37,6 +37,62 @@ describe('deleteFarmaco', () => {
         await deleteFarmaco(req, res)
 
         expect(Farmaco.findByIdAndDelete).toHaveBeenCalledWith(id)
+        expect(res.status).toHaveBeenCalledWith(404)
+        expect(res.json).toHaveBeenCalledWith({ message: errorMessage })
+    })
+})
+
+//Aggiorna Farmaco
+describe('updateFarmaco', () => {
+    test('Dovrebbe aggiornare il Farmaco tramite l\'id ricevuto in input e restituire il Farmaco aggiornato', async () => {
+        const id = '123'
+        const data = {
+            stock: 200
+        }
+        const req = {
+            params: {
+                id: id
+            },
+            body: data
+        }
+        const res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn()
+        }
+        const updatedFarmaco = {
+            _id: id,
+            ...data
+        }
+        jest.spyOn(Farmaco, 'findByIdAndUpdate').mockResolvedValueOnce(updatedFarmaco)
+
+        await updateFarmaco(req, res)
+
+        expect(Farmaco.findByIdAndUpdate).toHaveBeenCalledWith(id, data, { new: true })
+        expect(res.status).toHaveBeenCalledWith(200)
+        expect(res.json).toHaveBeenCalledWith(updatedFarmaco)
+    })
+
+    test('Dovrebbe impostare stato della richiesta a 404 e restituire un messaggio di errore se il Farmaco con dato id di input non è presente' , async () => {
+        const id = '123'
+        const data = {
+            stock: 200
+        }
+        const req = {
+            params: {
+                id: id
+            },
+            body: data
+        }
+        const res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn()
+        }
+        const errorMessage = 'Farmaco non trovato'
+        jest.spyOn(Farmaco, 'findByIdAndUpdate').mockRejectedValueOnce(new Error(errorMessage))
+
+        await updateFarmaco(req, res)
+
+        expect(Farmaco.findByIdAndUpdate).toHaveBeenCalledWith(id, data, { new: true })
         expect(res.status).toHaveBeenCalledWith(404)
         expect(res.json).toHaveBeenCalledWith({ message: errorMessage })
     })
