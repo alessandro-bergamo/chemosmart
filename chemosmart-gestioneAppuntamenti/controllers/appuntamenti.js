d84af8083b0a9966051a80e22086e752f52e1aa9
m21p99 <krasicjuventus.proka@gmail.com>
2023-02-17 14:27:56 +0100

    update  insertAppuntamento

--- chemosmart-gestioneAppuntamenti/controllers/appuntamenti.js
+++ chemosmart-gestioneAppuntamenti/controllers/appuntamenti.js
@@ -4,7 +4,38 @@ const Appuntamento = require('../models/appuntamento.js')
 //controller per inserire un appuntamento 
 exports.insertAppuntamento = async (req, res) => {
     const appuntamento = new Appuntamento(req.body)
-    
+
+    if (!appuntamento.cfPaziente || !appuntamento.farmaco || !appuntamento.dataInizio || !appuntamento.dataFine || !appuntamento.nome || !appuntamento.cognome) {
+        res.status(400).json()
+    }
+
+    /*
+        ** Implementata la regex per controllare se il codice Fiscale è valido.
+             ** Tramite il metodo test() verifichiamo se il codice Fiscale dell'appuntamento corrisponde alla regex
+    */
+    const regex = /[A-Z]{6}\d{2}[A-Z]\d{2}[A-Z]\d{3}[A-Z]/;
+    const cf = appuntamento.cfPaziente;
+    if (regex.test(cf)) {
+        res.status(400).json("Codice fiscale valido")
+    } else {
+        res.status(400).json("Codice fiscale non valido")
+    }
+
+    /*
+        ** Implementata la regex per controllare se il nome e cognome è valido.
+             ** Tramite il metodo test() verifichiamo se il nome e cognome corrisponde alla regex
+    */
+    const regexNameSurname = /^[a-zA-Z]+$/
+    const name = appuntamento.nome
+    const surname = appuntamento.cognome
+
+    if (regex.test(name) && regex.test(surname)) {
+        res.status(400).json("Input valido")
+    } else {
+        res.status(400).json("Input non valido")
+    }
+
+
     try {
         await appuntamento.save()
         res.status(201).json(appuntamento)
@@ -12,41 +43,41 @@ exports.insertAppuntamento = async (req, res) => {
         res.status(409).json({ message: error.message })
     }
 }
- //funzione per fromattare correttamente nome e cognome
+//funzione per fromattare correttamente nome e cognome
 function formattaParole(parole) {
     // Divido la stringa in due parole usando uno spazio come separatore
     const paroleArray = parole.split(" ");
     // Formatto ogni parola in modo che inizi con la maiuscola e poi abbia tutte le altre lettere in minuscolo
     const paroleFormattate = paroleArray.map(parola => {
-      const primaLettera = parola.charAt(0).toUpperCase();
-      const restoParola = parola.slice(1).toLowerCase();
-      return primaLettera + restoParola;
+        const primaLettera = parola.charAt(0).toUpperCase();
+        const restoParola = parola.slice(1).toLowerCase();
+        return primaLettera + restoParola;
     });
     // Unisco le parole formattate in una singola stringa separata da uno spazio
     const paroleFormattateStringa = paroleFormattate.join(" ");
     return paroleFormattateStringa;
-  }
-  
+}
+
 
 //controller per restituire tutti gli appuntamenti
 exports.getAllAppuntamenti = (req, res) => {
     const appuntamento = Appuntamento.find({})
-    .then(appuntamenti => {
-      const events = [];
-      for (const appuntamento of appuntamenti) {
-        events.push({
-          title: formattaParole(appuntamento.nome +' '+ appuntamento.cognome),
-          start: appuntamento.dataInizio,
-          end: appuntamento.dataFine,
-          id: appuntamento._id
+        .then(appuntamenti => {
+            const events = [];
+            for (const appuntamento of appuntamenti) {
+                events.push({
+                    title: formattaParole(appuntamento.nome + ' ' + appuntamento.cognome),
+                    start: appuntamento.dataInizio,
+                    end: appuntamento.dataFine,
+                    id: appuntamento._id
+                });
+            }
+            res.send(events);
+        })
+        .catch(error => {
+            console.error(error);
+            res.status(500).send(error);
         });
-      }
-      res.send(events);
-    })
-    .catch(error => {
-      console.error(error);
-      res.status(500).send(error);
-    });
 }
 
 //controller per restituire un appuntamento  in base all'id
@@ -77,12 +108,12 @@ exports.deleteAppuntamento = async (req, res) => {
 //controller per aggiornare un appuntamento in base all'id
 exports.updateAppuntamento = async (req, res) => {
     const id = req.params.id
-   
+
     const data = { ...req.body }
-    
+
     try {
-        const appuntamento = await Appuntamento.findByIdAndUpdate(id, data, {new:true}) //new ture serve per restituire effetivamente il json aggiornato
-        
+        const appuntamento = await Appuntamento.findByIdAndUpdate(id, data, { new: true }) //new ture serve per restituire effetivamente il json aggiornato
+
         res.status(200).json(appuntamento)
     } catch (error) {
         res.status(404).json({ message: error.message })
@@ -90,8 +121,8 @@ exports.updateAppuntamento = async (req, res) => {
 }
 
 //controller per l'instaziazione di un nuovo oggetto Appuntamento
-exports.createAppuntamento = async (req,res) => {
-    try{
+exports.createAppuntamento = async (req, res) => {
+    try {
         const appuntamento = new Appuntamento(req.body)
         res.status(201).json(appuntamento)
     } catch (error) {
--- chemosmart-gestioneAppuntamenti/tests/appuntamenti.test.js
+++ chemosmart-gestioneAppuntamenti/tests/appuntamenti.test.js
@@ -1,33 +1,76 @@
 // controllers/appuntamenti.test.js
-const Appuntamento = require('../models/appuntamento')
-const appuntamentiController = require('../controllers/appuntamenti')
 
+const Appuntamento = require('../models/appuntamento');
+const { insertAppuntamento } = require('../controllers/appuntamenti')
 
 jest.mock('../models/appuntamento.js')
+
 describe('insertAppuntamento', () => {
-  test('should create a new appuntamento and return it with status 201', async () => {
-    const appuntamento = {
-      cfPaziente: 'aa',
-      dataInizio: '2023-02-18',
-      dataFine: '2023-02-18',
-      farmaco: 'aspirina',
-    }
+  let req, res, appuntamento;
 
-    const req = { body: appuntamento }
-    const res = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn()
-    }
+  beforeEach(() => {
+    appuntamento = {
+      cfPaziente: 'RSSMRA80A01H501L',
+      farmaco: 'Ibuprofene',
+      dataInizio: new Date('2023-02-20T09:00:00Z'),
+      dataFine: new Date('2023-02-20T10:00:00Z'),
+      nome: 'Mario',
+      cognome: 'Rossi',
+      timestamps: true
+    };
+    req = { body: appuntamento };
+    res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
+  });
 
-    const savedAppuntamento = { ...appuntamento, save: jest.fn() }
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
 
-    Appuntamento.mockReturnValue(savedAppuntamento)
+  test('should return 400 if required fields are missing', async () => {
+    delete appuntamento.cfPaziente;
+    await insertAppuntamento(req, res);
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.json).toHaveBeenCalled();
+  });
 
-    await appuntamentiController.insertAppuntamento(req, res)
+  test('should return 400 if codice fiscale is not valid', async () => {
+    appuntamento.cfPaziente = 'invalid-cf';
+    await insertAppuntamento(req, res);
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.json).toHaveBeenCalled();
+  });
 
-    expect(Appuntamento).toHaveBeenCalledWith(appuntamento)
-    expect(savedAppuntamento.save).toHaveBeenCalled()
-    expect(res.status).toHaveBeenCalledWith(201)
-    expect(res.json).toHaveBeenCalledWith(savedAppuntamento)
-  })
-})
+  test('should return 400 if nome or cognome contain invalid characters', async () => {
+    appuntamento.nome = 'Mario1';
+    await insertAppuntamento(req, res);
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.json).toHaveBeenCalled();
+  });
+
+test('should create a new appuntamento and return it with status 201', async () => {
+
+  const savedAppuntamento = { ...appuntamento, save: jest.fn() }
+
+  Appuntamento.mockReturnValue(savedAppuntamento)
+
+  await insertAppuntamento(req, res)
+
+  expect(Appuntamento).toHaveBeenCalledWith(appuntamento)
+  expect(savedAppuntamento.save).toHaveBeenCalled()
+  expect(res.status).toHaveBeenCalledWith(201)
+  expect(res.json).toHaveBeenCalledWith(savedAppuntamento)
+});
+
+
+
+  test('should return 409 if saving fails', async () => {
+    const error = new Error('Database error');
+    const app = Appuntamento.mockReturnValueOnce({ save: jest.fn().mockRejectedValueOnce(error) });
+    await insertAppuntamento(req, res);
+    expect(Appuntamento).toHaveBeenCalledWith(appuntamento);
+    expect(app).toHaveBeenCalled();
+    expect(res.status).toHaveBeenCalledWith(409);
+    expect(res.json).toHaveBeenCalledWith({ message: error.message });
+  });
+
+});
