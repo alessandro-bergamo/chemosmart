53e8f07c4d7c29072e79a1e1c81edaff1b066c0b
AntonioNappi988 <74452384+AntonioNappi988@users.noreply.github.com>
2023-02-21 16:02:44 +0100

    Aggiunto Author e description per la funzione di test per i farmaci.

--- chemosmart-gestione-farmaci/tests/farmaci.test.js
+++ chemosmart-gestione-farmaci/tests/farmaci.test.js
@@ -3,6 +3,19 @@ const Farmaco = require('../models/farmaco')
 
 jest.mock('../models/farmaco');
 
+/*Restituisce Farmaco Aggiornato
+
+Author: Claudio de Palma
+
+Description:
+[TC_3.1_1] Il Farmaco non è presente nel database: questo test verifica il comportamento della funzione quando il farmaco che si intende aggiornare non è presente nel database. 
+In questo caso, la funzione deve restituire uno stato HTTP 400 con un messaggio che indica che il farmaco non è stato trovato.
+[TC_3.1_2] Il parametro stock non è valido: questo test verifica il comportamento della funzione quando il parametro "stock" del farmaco è un valore negativo. 
+In questo caso, la funzione deve restituire uno stato HTTP 400 con un messaggio che indica che il parametro stock non è valido.
+[TC_3.1_3] L'aggiornamento avviene con successo: questo test verifica il comportamento della funzione quando l'aggiornamento del farmaco avviene correttamente. 
+In questo caso, la funzione deve restituire uno stato HTTP 200 con il farmaco aggiornato come corpo della risposta.
+*/
+
 describe('updateFarmaco', () => {
     it('[TC_3.1_1] Il Farmaco non è presente nel database', async () => {
         const id = '123';
